eba2e478305bbe0195f9d1b60c264a06ccbf35b8
lerbal7 <leandro.r.balmaceda@gmail.com>
2020-06-09 23:27:37 -0300

    Working with props 2

--- App.js
+++ App.js
@@ -6,28 +6,17 @@ function App() {
   return (
     <div>
       <ContactCard 
-        name="Mr Whiskas" 
-        imgUrl="http://placekitten.com/300/200" 
-        phone="92014021" 
-        email="sdalknsalk@laknfakl.com" 
+        contact = {{name: "Mr Whiskas", imgUrl: "http://placekitten.com/300/200", phone:"92014021", email:"sdalknsalk@laknfakl.com" }}
       />
       <ContactCard 
-        name="Mr A" 
-        imgUrl="http://placekitten.com/300/100" 
-        phone="3453441" 
-        email="cvbvcbvcc@laknfakl.com" 
+        contact = {{name:"Mr A", imgUrl:"http://placekitten.com/300/100", phone:"3453441", email:"cvbvcbvcc@laknfakl.com" }}   
       />
       <ContactCard 
-        name="Mr Whiskas" 
-        imgUrl="http://placekitten.com/100/200" 
-        phone="7547542" 
-        email="sfsfsf@laknfakl.com" 
+        contact = {{name:"Mr Whiskas", imgUrl:"http://placekitten.com/100/200", phone:"7547542", email:"sfsfsf@laknfakl.com" }}
+        
       />
       <ContactCard 
-        name="Mr Whiskas" 
-        imgUrl="http://placekitten.com/600/100" 
-        phone="14141421" 
-        email="eqwrqr@laknfakl.com" 
+        contact = {{name:"Mr Whiskas", imgUrl:"http://placekitten.com/600/100" ,phone:"14141421" ,email:"eqwrqr@laknfakl.com" }}
       />
     </div>
   )
